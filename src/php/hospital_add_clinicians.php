a188650ecb3a1bdb2e2db479e47a8a6ff2525ee6
thilini.yashodha.demel <thilini.yashodha.demel@gmail.com>
2022-11-27 09:20:39 +0530

    New

--- src/php/hospital_add_clinicians.php
+++ src/php/hospital_add_clinicians.php
@@ -1,4 +1,3 @@
-<?php require_once('hospital_navbar.php'); ?>
 <?php
 require_once('../../config/connection.php');
 session_start();
@@ -8,6 +7,8 @@ if (!(isset($_SESSION['user_name']) && isset($_SESSION['hospital_name']) ))
 }
  ?>
 
+ 
 
-<?php include('../../public/html/hospital_patients.html'); ?>
+
+<?php include('../../public/html/hospital_add_clinicians.html'); ?>
 <?php require_once('hospital_footer.php'); ?>
